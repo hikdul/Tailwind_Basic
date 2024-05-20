8b1f884eaf75f4a41f99e67de374de4dbfff58a3
hikdul <hikdul.lio@gmail.com>
2024-05-20 17:44:28 -0500

    Up[Basic]: creando elemntos basicos y uso de colores

---
+++ 0.basic/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors:{
+                'azul-h': '#243cff',
+                'rojo-h': '#fc0320'
+            }
+        },
+    },
+    plugins: [],
+}
+
